27627435acb39c4d06e285f3b35eaa9e8cd9fa7b
Ryuki-4648 <hato_poppo73@yahoo.co.jp>
2024-09-15 15:33:25 +0900

    dayjs導入、Fontfamily追加

--- museum/src/app/layout.tsx
+++ museum/src/app/layout.tsx
@@ -1,4 +1,7 @@
+/* eslint-disable @next/next/no-page-custom-font */
 import type { Metadata } from "next";
+import Script from 'next/script'
+
 import { Inter } from "next/font/google";
 import "./globals.scss";
 
@@ -16,6 +19,40 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ja">
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Cabin:ital,wght@0,400..700;1,400..700&display=swap"
+          rel="stylesheet"
+        />
+        {/* Typekitのカスタムスクリプト */}
+        <Script id="typekit-script" strategy="beforeInteractive">
+          {`
+            (function(d) {
+              var config = {
+                kitId: 'yca6shm',
+                scriptTimeout: 3000,
+                async: true
+              },
+              h=d.documentElement,t=setTimeout(function(){
+                h.className=h.className.replace(/\\bwf-loading\\b/g,"")+" wf-inactive";
+              },config.scriptTimeout),tk=d.createElement("script"),f=false,s=d.getElementsByTagName("script")[0],a;
+              h.className+=" wf-loading";
+              tk.src='https://use.typekit.net/'+config.kitId+'.js';
+              tk.async=true;
+              tk.onload=tk.onreadystatechange=function(){
+                a=this.readyState;
+                if(f||a&&a!="complete"&&a!="loaded") return;
+                f=true;
+                clearTimeout(t);
+                try{Typekit.load(config)}catch(e){}
+              };
+              s.parentNode.insertBefore(tk,s);
+            })(document);
+          `}
+        </Script>
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   );
