0ef0e84afeae27d014c5c874199ef956dee21bad
Ryuki-4648 <hato_poppo73@yahoo.co.jp>
2024-09-15 15:33:02 +0900

    トップ：慣性スクロール導入（Lenis）、位置調整

--- museum/src/app/page.tsx
+++ museum/src/app/page.tsx
@@ -1,28 +1,92 @@
+"use client"
+/** 
+ * クライアントコンポーネントとして宣言
+ * use clientを使うとき：クライアントサイドでのみ動作する機能を使う場合
+ * 例）useEffect、useState、DOM操作、外部ライブラリのクライアントサイドでの実行
+ * 必要なのコンポーネントや処理に限定して使うのが良い
+ */
+
 import Image from "next/image";
+import dayjs from "dayjs";
+import Lenis from '@studio-freight/lenis'
+import { useEffect } from "react";
 
 export default function Home() {
+  /** 慣性スクロール */
+  useEffect(() => { // クライアントサイドでのみ実行
+    const lenis = new Lenis()
+    function raf(time: number) {
+      lenis.raf(time)
+      requestAnimationFrame(raf)
+    }
+    
+    requestAnimationFrame(raf)
+  }, []) // コンポーネントのマウント時にのみ実行
+
   return (
-    <main className="p-24 bg-bg01 h-screen">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="absolute left-1/2 -translate-1/2 top-4 leading-4 text-md">Out Profile and History</p>
-        <p className="absolute left-4 top-8">生い立ちと<br />プロフィール</p>
-        <h1 className="text-[6rem] w-full">OUT HISTORY</h1>
-        {/* <div>
-          <a href="" className="text-[16rem] rotate-90 leading-none absolute -left-80 top-1/2 -translate-y-1/2">GROOM</a>
-        </div>
-        <div>
-          <a href="" className="text-[16rem] rotate-90 leading-none absolute left-1/2 top-1/2 -translate-y-1/2">BRIDE</a>
-        </div> */}
-        <div>
-          <a href="" className="text-[16rem] rotate-90 leading-none">GROOM</a>
-        </div>
-        <div>
-          <a href="" className="text-[16rem] rotate-90 leading-none">BRIDE</a>
-        </div>
-        <p className="text-[8rem] absolute bottom-12 left-48">白石麻衣</p>
+    <main className="pt-36 pb-[20rem] bg-bg01 font-mono relative">
+
+        <p className="text-center text-[2rem] font-cabin leading-3 tracking-widest">Our Profile and History</p>
+        <p className="absolute left-24 top-[30rem] text-[8.2rem] font-ten">生い立ちと<br />プロフィール</p>
+        {/* <p className="text-[6rem] w-full text-center">INTRO</p> */}
+        {/* <p className="font-ten text-[3rem] opacity-20">皆様には ご健勝のこととお慶び申し上げます<br />このたび お越しいただき誠にありがとうございます<br />つきましては<br />ご挨拶をかねて心ばかりの小宴を催したいと思います</p> */}
+
+        <section className="absolute right-0 top-[80rem] flex" id="section-bride">
+          <p className="text-[11rem] mr-[24rem] font-ten w-full leading-none tracking-widest">
+            {process.env.NEXT_PUBLIC_BRIDE_NAME}
+          </p>
+          <Image
+            src="/prod/img_bride01.jpg"
+            alt="Picture of the author"
+            className="rounded-tl-[14rem]"
+            width={600}
+            height={300}
+          />
+          <div className="absolute -left-[0rem] top-[14rem]">
+            <ul className="text-[2.8rem] font-ten leading-loose">
+              <li>{dayjs(process.env.NEXT_PUBLIC_BRIDE_BIRTHDAY).format('YYYY年M月DD日')}生まれ</li>
+              <li>出身：{process.env.NEXT_PUBLIC_BRIDE_BIRTH_PLACE}</li>
+              <li>血液型：{process.env.NEXT_PUBLIC_BRIDE_BLOOD_TYPE}</li>
+              <li>職業：{process.env.NEXT_PUBLIC_BRIDE_OCCUPATION}</li>
+              <li>趣味：{process.env.NEXT_PUBLIC_BRIDE_HOBBY}</li>
+              <li>好きなもの：{process.env.NEXT_PUBLIC_BRIDE_FAVORITE01}<br />{process.env.NEXT_PUBLIC_BRIDE_FAVORITE02}</li>
+            </ul>
+          </div>
+          <div className="absolute right-[22rem] top-[16rem] block rotate-90 hover:text-accent01 duration-500">
+            <a href="" className="text-[16.5rem] leading-none font-cabin tracking-wider">
+              BRIDE
+            </a>
+          </div>
+        </section>
+
+        <section className="mt-[120rem] flex items-baseline" id="section-groom">
+          <Image
+            src="/prod/img_groom01.jpg"
+            alt="Picture of the author"
+            className="mt-[40rem] rounded-tr-[14rem]"
+            width={600}
+            height={300}
+          />
+          <div className="absolute bottom-[62rem] left-[54rem] hover:text-accent01 duration-500">
+            <a href="" className="text-[16rem] leading-none font-cabin">GROOM</a>
+          </div>
+          <p className="text-[9.5rem] font-ten c-textVertical ml-[2.5rem] leading-none tracking-widest">
+            {process.env.NEXT_PUBLIC_GROOM_NAME}
+          </p>
+          <div className="absolute ml-[55rem] bottom-[22rem]">
+            <ul className="text-[2.8rem] font-ten leading-loose">
+              <li>{dayjs(process.env.NEXT_PUBLIC_GROOM_BIRTHDAY).format('YYYY年M月DD日')}生まれ</li>
+              <li>出身：{process.env.NEXT_PUBLIC_GROOM_BIRTH_PLACE}</li>
+              <li>血液型：{process.env.NEXT_PUBLIC_GROOM_BLOOD_TYPE}</li>
+              <li>職業：{process.env.NEXT_PUBLIC_GROOM_OCCUPATION}</li>
+              <li>趣味：{process.env.NEXT_PUBLIC_GROOM_HOBBY}</li>
+              <li>好きなもの：{process.env.NEXT_PUBLIC_GROOM_FAVORITE01}<br />{process.env.NEXT_PUBLIC_GROOM_FAVORITE02}</li>
+            </ul>
+          </div>
+        </section>
         
-        <p className="text-[8rem] absolute bottom-12 right-48">与田祐希</p>
-      </div>
+      
+
     </main>
   );
 }
