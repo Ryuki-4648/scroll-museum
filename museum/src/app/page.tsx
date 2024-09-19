93676d702d7d660773cf7cbc68870cfdeb634202
Ryuki-4648 <hato_poppo73@yahoo.co.jp>
2024-09-19 22:21:50 +0900

    スクロール領域に入ったらプロフィール文章をタイピング風にアニメーションさせる

--- museum/src/app/page.tsx
+++ museum/src/app/page.tsx
@@ -9,10 +9,41 @@
 import Image from "next/image";
 import dayjs from "dayjs";
 import Lenis from '@studio-freight/lenis'
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 import { useRouter } from "next/navigation"; // next/routerの場合NextRouter was not mounted.のエラーとなる
+import { useInView } from "react-intersection-observer";
+
+
+const useTypingObserver = () => {
+  const observer = useRef<IntersectionObserver | null>(null);
+
+  useEffect(() => {
+    observer.current = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry, index) => {
+          if (entry.isIntersecting) {
+            setTimeout(() => {
+              entry.target.classList.add("is-visible")
+            }, index * 30000)
+            observer.current?.unobserve(entry.target)
+          }
+        });
+      },
+      { threshold: 0.8 }
+    );
+
+    const items = document.querySelectorAll(".typing-item");
+    items.forEach((item) => observer.current?.observe(item));
+
+    return () => {
+      observer.current?.disconnect();
+    };
+  }, []);
+};
 
 export default function Home() {
+
+  useTypingObserver();
 
   const router = useRouter()
 
@@ -42,8 +73,13 @@ export default function Home() {
   return (
     <main className="pt-36 pb-[80rem] bg-bg01 font-mono relative">
 
-      <p className="text-center text-[2rem] font-cabin leading-3 tracking-widest">Our Profile and History</p>
-      <p className="absolute left-24 top-[30rem] text-[8.2rem] font-ten">生い立ちと<br />プロフィール</p>
+      <p className="text-center text-[2rem] font-cabin leading-3 tracking-widest">
+        Our Profile and History
+      </p>
+      <p className="absolute left-24 top-[30rem] text-[8.2rem] font-ten">
+        生い立ちと<br />プロフィール
+      </p>
+      <p className="absolute left-24 top-[30rem] text-[8.2rem] font-ten fade-in">生い立ちと<br />プロフィール</p>
       {/* <p className="text-[6rem] w-full text-center">INTRO</p> */}
       {/* <p className="font-ten text-[3rem] opacity-20">皆様には ご健勝のこととお慶び申し上げます<br />このたび お越しいただき誠にありがとうございます<br />つきましては<br />ご挨拶をかねて心ばかりの小宴を催したいと思います</p> */}
 
@@ -72,7 +108,8 @@ export default function Home() {
             <li>血液型：{process.env.NEXT_PUBLIC_GROOM_BLOOD_TYPE}</li>
             <li>職業：{process.env.NEXT_PUBLIC_GROOM_OCCUPATION}</li>
             <li>趣味：{process.env.NEXT_PUBLIC_GROOM_HOBBY}</li>
-            <li>好きなもの：{process.env.NEXT_PUBLIC_GROOM_FAVORITE01}<br />{process.env.NEXT_PUBLIC_GROOM_FAVORITE02}</li>
+            <li>好きなもの：{process.env.NEXT_PUBLIC_GROOM_FAVORITE01}</li>
+            <li>{process.env.NEXT_PUBLIC_GROOM_FAVORITE02}</li>
           </ul>
         </div>
       </section>
@@ -93,13 +130,13 @@ export default function Home() {
           height={300}
         />
         <div className="absolute left-[6rem] top-[14rem]">
-          <ul className="text-[2.8rem] font-ten leading-loose">
-            <li>{dayjs(process.env.NEXT_PUBLIC_BRIDE_BIRTHDAY).format('YYYY年M月DD日')}生まれ</li>
-            <li>出身：{process.env.NEXT_PUBLIC_BRIDE_BIRTH_PLACE}</li>
-            <li>血液型：{process.env.NEXT_PUBLIC_BRIDE_BLOOD_TYPE}</li>
-            <li>職業：{process.env.NEXT_PUBLIC_BRIDE_OCCUPATION}</li>
-            <li>趣味：{process.env.NEXT_PUBLIC_BRIDE_HOBBY}</li>
-            <li>好きなもの：{process.env.NEXT_PUBLIC_BRIDE_FAVORITE01}<br />{process.env.NEXT_PUBLIC_BRIDE_FAVORITE02}</li>
+          <ul className="text-[2.8rem] font-ten leading-loose typing-list">
+            <li className="typing-item">{dayjs(process.env.NEXT_PUBLIC_BRIDE_BIRTHDAY).format('YYYY年M月DD日')}生まれ</li>
+            <li className="typing-item">出身：{process.env.NEXT_PUBLIC_BRIDE_BIRTH_PLACE}</li>
+            <li className="typing-item">血液型：{process.env.NEXT_PUBLIC_BRIDE_BLOOD_TYPE}</li>
+            <li className="typing-item">職業：{process.env.NEXT_PUBLIC_BRIDE_OCCUPATION}</li>
+            <li className="typing-item">趣味：{process.env.NEXT_PUBLIC_BRIDE_HOBBY}</li>
+            <li className="typing-item">好きなもの：{process.env.NEXT_PUBLIC_BRIDE_FAVORITE01}<br />{process.env.NEXT_PUBLIC_BRIDE_FAVORITE02}</li>
           </ul>
         </div>
         <div className="absolute right-[22rem] top-[16rem] block rotate-90 hover:text-accent01 duration-500">
